3772c22347db8b0a197810d6642080cc8925b8cc
Juan José Cuervo <juanjosecu234@gmail.com>
2026-04-13 18:48:38 -0500

    Clase Guest implemntada.

--- app/model/hotel.py
+++ app/model/hotel.py
@@ -8,7 +8,16 @@ from app.services.util import (generate_unique_id, date_lower_than_today_error,
 
 
 # TODO: Implement Guest class here
-#hola
+@dataclass
+class Guest:
+    REGULAR = "regular"
+    VIP = "vip"
+    name: str
+    email: str
+    type: str
+
+    def __str__(self):
+        return f"Guest {self.name} ({self.email}) of type {self.type_}"
 
 # TODO: Implement Reservation class here
 
